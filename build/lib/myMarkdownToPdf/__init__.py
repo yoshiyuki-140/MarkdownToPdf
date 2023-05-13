cbbd59057a4790e0336b7799c3659ec5eb02b933
yoshiyuki-140 <c1221251@st.kanazawa-it.ac.jp>
2023-05-13 20:13:55 +0900

    I tidy this repo

--- build/lib/myMarkdownToPdf/__init__.py
+++
@@ -1,1 +0,0 @@
-from myMarkdownToPdf.myMarkdownTopdf import *
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='myMarkdownToPdf',
+    name='MarkdownToPdf',
     version='1.0',
     packages=find_packages()
 )
